ff6ac24075e8cded5e679e11aa5ca8c0061b65d5
ro <rhodri.mativo@gmail.com>
2025-07-22 23:25:52 -0400

    refactor: :art: fix the svg shadow cropping and trying out glass stlye

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,17 @@ import Image from 'next/image';
 export default function Page () {
 	return (
 		<main className="flex min-h-screen flex-col p-6">
-			<div className="flex h-20 shrink-0 items-end rounded-lg bg-blue p-4 md:h-52">
+
+
+			<div className="flex h-20 shrink-0 items-end rounded-2xl border border-white/20 bg-blue/70 p-4 shadow-xl backdrop-blur-lg backdrop-saturate-150 md:h-52">
 				<BellevueLogo className='text-base0'/>
 			</div>
-			<div className="mt-4 flex grow flex-col rounded-lg bg-mantle p-5 md:flex-row">
+
+
+		   <div
+			   className="mt-4 flex grow flex-col rounded-2xl border border-overlay0/20 bg-mantle/40 p-5 shadow-xl backdrop-blur-lg backdrop-saturate-150 md:flex-row"
+		   >
+
 
 				{/* login side */}
 				 <div className="flex flex-col justify-center px-6 py-10 md:w-2/5 md:px-5">
@@ -25,17 +32,16 @@ export default function Page () {
 						<span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
 					</Link>
 				</div>
-				{/* image side */}
-				<div className="relative flex flex-auto md:-ml-8">
-					<Image
-						objectFit='contain'
-						src="/svg/opengraph-image.svg"
-						alt="Open Graph Image"
-						// height={315}
-						// width={620}
-						fill
-					/>
-				</div>
+			   {/* image side */}
+			   <div className="relative flex flex-auto overflow-visible md:-ml-8">
+				   <Image
+					   src="/svg/opengraph-image.svg"
+					   alt="Open Graph Image"
+					   width={1240}
+					   height={630}
+					   style={{ width: '100%', height: 'auto', maxWidth: '100%' }}
+				   />
+			   </div>
 			</div>
 		</main >
 	);
