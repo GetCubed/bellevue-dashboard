7501b0a4b783e1ef9ffe8fcab7c82cdde80d958a
ro <rhodri.mativo@gmail.com>
2025-07-23 22:57:11 -0400

    feat: :chart_with_upwards_trend: create customers page

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -18,12 +18,12 @@ export default async function CustomersTable ({
 			<div className="mt-6 flow-root">
 				<div className="overflow-x-auto">
 					<div className="inline-block min-w-full align-middle">
-						<div className="overflow-hidden rounded-md bg-gray-50 p-2 md:pt-0">
+						<div className="overflow-hidden rounded-md bg-surface1 p-2 md:pt-0">
 							<div className="md:hidden">
 								{customers?.map((customer) => (
 									<div
 										key={customer.id}
-										className="mb-2 w-full rounded-md bg-white p-4">
+										className="mb-2 w-full rounded-md bg-surface1 p-4">
 										<div className="flex items-center justify-between border-b pb-4">
 											<div>
 												<div className="mb-2 flex items-center">
@@ -58,8 +58,8 @@ export default async function CustomersTable ({
 									</div>
 								))}
 							</div>
-							<table className="hidden min-w-full rounded-md text-gray-900 md:table">
-								<thead className="rounded-md bg-gray-50 text-left text-sm font-normal">
+							<table className="hidden min-w-full rounded-md text-text md:table">
+								<thead className="rounded-md bg-surface1 text-left text-sm font-normal">
 									<tr>
 										<th scope="col"
 											className="px-4 py-5 font-medium sm:pl-6">
@@ -84,11 +84,11 @@ export default async function CustomersTable ({
 									</tr>
 								</thead>
 
-								<tbody className="divide-y divide-gray-200 text-gray-900">
+								<tbody className="divide-y divide-surface1 bg-surface0 text-text">
 									{customers.map((customer) => (
 										<tr key={customer.id}
 											className="group">
-											<td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
+											<td className="whitespace-nowrap  py-5 pl-4 pr-3 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
 												<div className="flex items-center gap-3">
 													<Image
 														src={customer.image_url}
@@ -99,16 +99,16 @@ export default async function CustomersTable ({
 													<p>{customer.name}</p>
 												</div>
 											</td>
-											<td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+											<td className="whitespace-nowrap  px-4 py-5 text-sm">
 												{customer.email}
 											</td>
-											<td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+											<td className="whitespace-nowrap  px-4 py-5 text-sm">
 												{customer.total_invoices}
 											</td>
-											<td className="whitespace-nowrap bg-white px-4 py-5 text-sm">
+											<td className="whitespace-nowrap  px-4 py-5 text-sm">
 												{customer.total_pending}
 											</td>
-											<td className="whitespace-nowrap bg-white px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
+											<td className="whitespace-nowrap px-4 py-5 text-sm group-first-of-type:rounded-md group-last-of-type:rounded-md">
 												{customer.total_paid}
 											</td>
 										</tr>
